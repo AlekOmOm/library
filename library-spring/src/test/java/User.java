8627e8f5b7fad0696178f08dd1b8fdfb6ee2b8ab
Aleksander Hvid Ellegård <142972647+AlekOmOm@users.noreply.github.com>
2024-10-29 09:37:46 +0100

    Remove `MainTest.java` and update `User.java` with annotations
    Deleted `MainTest.java` as it contained unused and incomplete code. Updated `User.java` to include `NoArgsConstructor` and `AllArgsConstructor` annotations, improving the code's readability and maintainability.

--- library-spring/src/test/java/User.java
+++ library-spring/src/test/java/User.java
@@ -2,12 +2,16 @@
 
 import com.Alek0m0m.library.jpa.BaseEntity;
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class User extends BaseEntity {
 
     private String name;

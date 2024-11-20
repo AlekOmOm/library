58f1f03283d224748933667cdc324673e6c79136
AlekOmOm <142972647+AlekOmOm@users.noreply.github.com>
2024-11-20 18:01:59 +0100

    Refactor type parameters and add auto-increment reset
    Refactor the type parameters in BaseServiceInterface to standardize the order of generics. Additionally, add a method in BaseRepository to reset the auto-increment value for entity tables.

--- library-spring/src/main/java/com/Alek0m0m/library/spring/web/mvc/BaseRepository.java
+++ library-spring/src/main/java/com/Alek0m0m/library/spring/web/mvc/BaseRepository.java
@@ -3,9 +3,19 @@ package com.Alek0m0m.library.spring.web.mvc;
 
 import com.Alek0m0m.library.jpa.BaseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface BaseRepository<T extends BaseEntity> extends JpaRepository<T, Long> {
 
+
+    @Modifying
+    @Transactional
+    @Query(value = "ALTER TABLE #{#entityName} AUTO_INCREMENT = 1", nativeQuery = true)
+    void resetAutoIncrement();
+
 }
+
--- library-spring/src/main/java/com/Alek0m0m/library/spring/web/mvc/BaseServiceInterface.java
+++ library-spring/src/main/java/com/Alek0m0m/library/spring/web/mvc/BaseServiceInterface.java
@@ -8,12 +8,11 @@ import java.io.Serializable;
 import java.util.List;
 
 @Service
-public interface BaseServiceInterface<T extends BaseEntity, R extends BaseEntityDTO<T>> {
+public interface BaseServiceInterface< R extends BaseEntityDTO<T>, T extends BaseEntity> {
 
     BaseRepository<T> getRepository();
 
     R save(BaseEntityDTO<T> entityDTO);
-
 
     List<R> findAll();
     R findById(long id);
